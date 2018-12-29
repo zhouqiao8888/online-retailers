7fb460530be332f588eead7a5c39ec7ce256c7b3
zhouqiao8888 <1016010419@njupt.edu.cn>
2018-12-29 20:37:16 +0800

    用户登陆模块设计

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -1,5 +1,7 @@
 package com.mmall.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.mmall.pojo.User;
 
 public interface UserMapper {
@@ -14,4 +16,16 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+    
+    //检查当前用户名是否存在
+    int checkUsername(String username);	
+    
+    //检查当前eamil是否存在
+    int checkUserEmail(String email);	
+    
+    //返回当前用户信息
+    User selectLoginUser(@Param("username") String username, @Param("password") String password);
+    
+    //返回用户密保问题
+    String selectSecurityQuestion(String username);
 }
